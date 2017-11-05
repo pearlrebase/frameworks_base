0abb08bdd99692bf6c90a5d3b50167532261beec
Steve Kondik <steve@cyngn.com>
2017-11-05 16:37:52 +0100

    SystemUI: Add LiveDisplay tile
    Author: Steve Kondik <steve@cyngn.com>
    Date:   Sun Nov 5 16:37:52 2017 +0100
    
        SystemUI: LiveDisplay tile
    
        Change-Id: I53d492e5cb9998268104d5750705aa5ed55d9658
        Signed-off-by: Joey Rizzoli <joey@lineageos.org>
    
    Author: Unpublished <unpublished@gmx.net>
    Date:   Fri Dec 29 23:00:01 2017 +0100
    
        livedisplay: Allow tile creation before boot completed phase [2/2]
    
        Change-Id: Ibef44d4e07da9baf296796515288c9d42aba8608
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Thu Feb 1 16:39:16 2018 +0000
    Edit:   Refactor for Oreo
    
    Change-Id: Ibef44d4e07da9baf296796515288c9d42aba8608
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Thu Feb 1 17:01:43 2018 +0000
    
        LiveDisplayTile: Avoid NPE during boot up phase
    
         * During boot, with LineageHW not yet initialized there is no way to
           check the current LiveDisplay mode. In such cases, fallback to AUTO.
    
        Change-Id: Ifc004c86dfd68f73b8a362a1c833cbc3e7397811
    
    Change-Id: Ibef44d4e07da9baf296796515288c9d42aba8608
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -44,6 +44,7 @@ import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.HWKeysTile;
 import com.android.systemui.qs.tiles.IntentTile;
+import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LteTile;
 import com.android.systemui.qs.tiles.NavBarTile;
@@ -174,6 +175,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new PieTile(mHost);
             case "navbar":
                 return new NavBarTile(mHost);
+            case "livedisplay":
+                return new LiveDisplayTile(mHost);
         }
 
         // Intent tiles.
