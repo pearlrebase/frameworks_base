c95f3d9dda5b4d7d5bf98ab2041d228cb38d8921
sagarrokade006 <sagarrokade006@gmail.com>
2019-08-20 19:06:56 +0000

    Refractor for Pearl
    Change-Id: I1332f1071a8c9375c4b00b4e33f37ffc2d545a9a

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
@@ -131,7 +131,7 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.CUSTOM_SETTINGS;
+        return MetricsEvent.PEARL;
     }
 
     private void startCountDown(long duration) {
